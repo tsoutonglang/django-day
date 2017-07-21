471021786a67846cd2f5c2f8971c0d5bda2c5461
tsoutonglang <tsoutonglang12@gmail.com>
2017-07-21 13:34:25 -0500

    added the blog.urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+]
+
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
